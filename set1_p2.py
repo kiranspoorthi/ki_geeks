e3370a8243097e9bdd48625632f52f8573c36b71
kiranspoorthi <39819846+kiranspoorthi@users.noreply.github.com>
2018-07-12 14:27:23 +0530

    Add files via upload

---
+++ set1_p2.py
@@ -0,0 +1,11 @@
+n=int(input())
+if(n%2==0 ):
+    if(n>0):
+        print("Even")
+    else:
+        print("Invalid")
+elif(n%2!=0):
+    if(n>0):
+        print("Odd")
+    else:
+        print("Invalid")
